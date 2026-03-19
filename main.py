e02474a3096303c0a9d9045a0862b1703c0f3e60
theokng <theo.konig@efrei.net>
2026-03-19 15:41:27 +0100

    adapted to new format

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-test_truth_table = [
-    [0, 0],
-    [2, 0],
-    ["X", "X"],
-    [0, 4],
-    ["X", "X"],
-] #expl taken frocdm auto5, to test while we cannot load from file
-
 def read_txt(filename):
     with open(filename) as f:
         nbSymbols = f.readline()
@@ -28,8 +20,8 @@ def read_txt(filename):
                 print(ord(x[1]))
                 print(int(x[0]))
                 truthTable[int(x[0])][ord(x[1])-97].append(int(x[2]))  #truthTable[nodeA][transitionNumber(a = 0, z=26)] = nodeB
-        return truthTable
-
+        return nbSymbols, nbState, initialStates, finalStates, truthTable
+    
 def printTruthTable(truthTable):
         ## NB : The code below was written using Gen AI, as it is only visual and requires no logic
         print("\nTruth Table:")
@@ -40,34 +32,34 @@ def printTruthTable(truthTable):
         for i, row in enumerate(truthTable):
             print(f"{i:>5} | {' | '.join(f'{str(val):>4}' for val in row)}")
         print("-" * len(header))
-    
 
-alpha_size = 2 #size of the alphabet, will be read from file
-nb_states = 5 #number of states, will be read from file
-
-def is_complete(auto):
+def is_complete(truthTable):
     is_comp = True
-    for row in (auto):
-        if "X" in row:
+    for row in (truthTable):
+        if "-" in row:
             is_comp = False
             break
     return is_comp
 
-def completion(auto):
-    for i in range(nb_states):
-        for j in range(alpha_size):
-            if auto[i][j] == "X":
-                auto[i][j] = -1 #-1 is sink state, on replace après
-    sink_state = []
-    for i in range(alpha_size):
-        sink_state.append(-1)
-    auto.append(sink_state)
+def completion(nbSymbols, nbState, truthTable):
+    for i in range(nbState):
+        for j in range(nbSymbols):
+            if truthTable[i][j] == "-":
+                truthTable[i][j] = -1 #-1 is sink state, on replace après
+    sink_state = [-1 for i in range(nbSymbols)]
+    truthTable.append(sink_state)
+
+
+def is_deterministic(nbSymbols, nbState, initialStates, truthTable):
+    if len(initialStates) > 1:
+        return False
+    
 
 
 
-print(test_truth_table)
-print(is_complete(test_truth_table))
-completion(test_truth_table)
-print(test_truth_table)
-truthTable = read_txt("test_automata/test_fa07.txt")
-printTruthTable(truthTable)
+
+nbSymbols, nbState, initialStates, finalStates, truthTable = read_txt("test_automata/test_fa07.txt")
+print(is_complete(truthTable))
+completion(nbSymbols, nbState, truthTable)
+print(is_complete(truthTable))
+print(truthTable)
