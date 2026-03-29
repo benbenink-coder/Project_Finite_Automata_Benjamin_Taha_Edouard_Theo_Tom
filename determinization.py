308a2b048def736a1853e02fbdac956ae8692d08
theokng <theo.konig@efrei.net>
2026-03-29 16:53:04 +0200

    added comments

--- determinization.py
+++ determinization.py
@@ -13,9 +13,12 @@ def is_deterministic(initialStates, truthTable):
     print("* Deterministic : " + str(is_det))
     return is_det
 
-def sls(arr):
-    return sorted(list(set(arr))) #remove duplicates and sort it asc so [2,1,3] and [3,2,1] etc.. are considered same
 
+#remove duplicates and sort it asc so [2,1,3] and [3,2,1] etc.. are considered same
+def sls(arr):
+    return sorted(list(set(arr))) 
+
+#finds the epsilon closure and fills them directly in epsCl
 def eps_closure(nbSymbols, truthTable, indice, epsCl):
     closure = {indice}
     stack = [indice]
@@ -29,11 +32,15 @@ def eps_closure(nbSymbols, truthTable, indice, epsCl):
     epsCl[indice] = ".".join(str(x) for x in sorted(list(closure)))
     return epsCl[indice]
 
+#determinizes the automaton
+# It creates a new truth table from scratch, and returns it with new state names.
+# To keep track on how they were called before merging, we use indicesToName, which is displayed
 def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
     newTruthTable = []
     toDo = []
     indicesToName = []
 
+    #checks if the automaton has epsilon transitions
     needEps = False
     for row in truthTable:
         if len(row[nbSymbols]) > 0:
@@ -41,6 +48,7 @@ def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
              needEps = True
              break
     
+    #computes the epsilon closures if needed
     if needEps:
         epsCl = ["" for i in range(nbState)]
         for i in range(nbState):
@@ -50,7 +58,7 @@ def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
     for i, group in enumerate(epsCl):
         print(f"  {i}' = {''.join(group)}")
     
-
+    #combine all the inital states to one single
     start_state = []
     if needEps:
         for s in initialStates:
@@ -58,17 +66,20 @@ def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
     else:
         start_state = [int(x) for x in initialStates]
 
-    newInitialStates = ["0"]
+    newInitialStates = ["0"] #as it will w=hae only one initial state, it will always be the first one (0)
     newFinalStates = []
     
     start_state = sls(start_state)
     toDo.append(start_state)
 
+    #main processing
     indice = 0
     while indice < (len(toDo)):
+        #simply to display a more readable name 
         stateName = ".".join(str(s) for s in toDo[indice])
         indicesToName.append(stateName)
 
+        #if it contains a final state, it is final
         if any(str(e) in finalStates for e in toDo[indice]):
             newFinalStates.append(str(indice))
 
@@ -79,6 +90,7 @@ def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
             for j in toDo[indice]:
                 trans.extend(truthTable[int(j)][i])
 
+            #find the states it will transition to (simply the combination)
             if needEps:
                 eps_trans = []
                 for t in trans:
@@ -87,6 +99,7 @@ def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
             else:
                 trans = sls([int(x) for x in trans])
 
+            #if a new state is created, which is not processed, we add it
             if len(trans) > 0:
                 if trans not in toDo:
                     toDo.append(trans)
@@ -94,6 +107,7 @@ def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
             else:
                 target_index = []
 
+            #we add the merged state
             stateRow.append(target_index)
 
         stateRow.append([])
