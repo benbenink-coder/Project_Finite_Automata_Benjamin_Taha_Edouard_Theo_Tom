28a1f2ed3a6ab3b88ea6f4591c6ccae6bdde1320
theokng <theo.konig@efrei.net>
2026-03-29 13:02:23 +0200

    added epsilon support for determinization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from queue import Empty
 import time
 
 
@@ -124,17 +125,54 @@ def is_deterministic(initialStates, truthTable):
     return is_det
 
 def sls(arr):
-    return sorted(list(set(arr)))
+    return sorted(list(set(arr))) #remove duplicates and sort it asc so [2,1,3] and [3,2,1] etc.. are considered same
+
+def eps_closure(nbSymbols, truthTable, indice, epsCl):
+    closure = {indice}
+    stack = [indice]
+    while stack:
+        curr = stack.pop()
+        # Ensure we access the epsilon column correctly
+        for s in truthTable[int(curr)][nbSymbols]:
+            if int(s) not in closure:
+                closure.add(int(s))
+                stack.append(int(s))
+    epsCl[indice] = ".".join(str(x) for x in sorted(list(closure)))
+    return epsCl[indice]
 
 def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
     newTruthTable = []
     toDo = []
     indicesToName = []
 
-    newInitialStates = [".".join(str(s) for s in sls(initialStates))]
+    needEps = False
+    for row in truthTable:
+        if len(row[nbSymbols]) > 0:
+             #eps_closure
+             needEps = True
+             break
+    
+    if needEps:
+        epsCl = ["" for i in range(nbState)]
+        for i in range(nbState):
+            eps_closure(nbSymbols, truthTable, i, epsCl)
+        print(epsCl) 
+    
+
+    start_state = []
+    if needEps:
+        for s in initialStates:
+            start_state.extend([int(x) for x in epsCl[int(s)].split(".") if x != ""])
+    else:
+        start_state = [int(x) for x in initialStates]
+
+    newInitialStates = ["0"]
     newFinalStates = []
     
-    toDo.append(sls(initialStates))
+    start_state = sls(start_state)
+    toDo.append(start_state)
+
+    print(toDo)
 
     indice = 0
     while indice < (len(toDo)):
@@ -151,14 +189,25 @@ def determinize(nbSymbols, nbState, initialStates, finalStates, truthTable):
             for j in toDo[indice]:
                 trans.extend(truthTable[int(j)][i])
 
-            trans = sls(trans) #remove duplicates and sort it asc so [2,1,3] and [3,2,1] etc.. are considered same
+            if needEps:
+                eps_trans = []
+                for t in trans:
+                    eps_trans.extend([int(x) for x in epsCl[int(t)].split(".") if x != ""])
+                trans = sls(eps_trans)
+                print(trans)
+            else:
+                trans = sls([int(x) for x in trans])
 
-            if trans not in toDo:
-                toDo.append(trans)
+            if len(trans) > 0:
+                if trans not in toDo:
+                    toDo.append(trans)
+                target_index = [toDo.index(trans)]
+            else:
+                target_index = []
 
-            target_index = toDo.index(trans)
             stateRow.append(target_index)
 
+        stateRow.append([])
         newTruthTable.append(stateRow)
         indice +=1
     print(indicesToName)
