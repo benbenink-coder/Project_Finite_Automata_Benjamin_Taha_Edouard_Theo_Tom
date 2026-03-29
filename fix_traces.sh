391eb67b0b010c54cf202adac27c0325cad68b95
benbenink-coder <benbenink@gmail.com>
2026-03-29 23:38:36 +0200

    Delete fix_traces.sh

--- fix_traces.sh
+++
@@ -1,6 +0,0 @@
-for i in 2; do
-    num=$(printf "%02d" $i)
-    rm "traces/trace_fa${num}.txt"
-    expect run_traces_with_input.exp $i > /dev/null 2>&1
-    sed -i '' 's/\r//g' "traces/trace_fa${num}.txt"
-done
